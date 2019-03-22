289caeaf95bf14c2bcd8c291587f1496eaef862f
Magdalena Navarro Torres Arpi <magdalena.nta@gmail.com>
2019-03-22 17:59:35 +0000

    Added SFARI genes info, changed 2D plots to selectable_scatter_plot

--- Gandal/RNAseq/consensus_clustering.R
+++ Gandal/RNAseq/consensus_clustering.R
@@ -6,8 +6,8 @@ library(ConsensusClusterPlus)
 
 ##### Load and transform data ###########################################################
 
-load('./working_data/RNAseq_ASD_4region_DEgenes_adj_pval.Rdata')
-#load('RNAseq_ASD_4region_normalized.Rdata')
+#load('./working_data/RNAseq_ASD_4region_normalized.Rdata')
+load('./working_data/RNAseq_ASD_4region_DEgenes_adj_pval_lfc.Rdata')
 
 reduce_dim_datExpr = function(datExpr, datMeta, var_explained=0.98){
   
@@ -34,18 +34,42 @@ rm(datSeq, datProbes, reduce_dim_datExpr, reduce_dim_output, datExpr)
 ##### Clustering ########################################################################
 
 # # Consensus Clustering
-# cc_output = datExpr_redDim %>% as.matrix %>% t %>%
-#   ConsensusClusterPlus(maxK=10, reps=5, seed=123, title='R_markdowns/clustering_genes_03_19/cc_l1', plot='png')
+# cc_gene_list = sample(rownames(datExpr_redDim), 10000)
+# cc_output = datExpr_redDim[cc_gene_list,] %>% as.matrix %>% t %>%
+#   ConsensusClusterPlus(maxK=10, reps=5, seed=123, title='R_markdowns/clustering_genes_03_08/cc_l1', plot='png')
 # 
-# save(cc_output, file='R_markdowns/clustering_genes_03_19/cc_l1/cc_output.RData')
+# save(cc_output, cc_gene_list, file='R_markdowns/clustering_genes_03_08/cc_l1/cc_output.RData')
+# 
+# load('R_markdowns/clustering_genes_03_08/cc_l1/cc_output.RData')
+# cc_clusters = cc_output[[4]]$consensusClass
+# 
+# cc_output_c1 = datExpr_redDim[cc_gene_list,] %>% filter(cc_clusters==1) %>% as.matrix %>% t %>%
+#    ConsensusClusterPlus(maxK=8, reps=50, seed=123, title='R_markdowns/clustering_genes_03_08/cc_l2_1/', plot='png')
+# save(cc_output_c1, file='R_markdowns/clustering_genes_03_08/cc_l2_1/cc_output.RData')
+# 
+# cc_output_c2 = datExpr_redDim[cc_gene_list,] %>% filter(cc_clusters==2) %>% as.matrix %>% t %>%
+#   ConsensusClusterPlus(maxK=8, reps=50, seed=123, title='R_markdowns/clustering_genes_03_08/cc_l2_2/', plot='png')
+# save(cc_output_c2, file='R_markdowns/clustering_genes_03_08/cc_l2_2/cc_output.RData')
 
-load('R_markdowns/clustering_genes_03_19/cc_l1/cc_output.RData')
+# Removing first PC
+# new_colnames = colnames(datExpr_redDim)[-ncol(datExpr_redDim)]
+# datExpr_redDim = datExpr_redDim %>% dplyr::select(-PC1)
+# colnames(datExpr_redDim) = new_colnames
+# 
+# # Consensus Clustering
+# cc_output = datExpr_redDim %>% as.matrix %>% t %>%
+#   ConsensusClusterPlus(maxK=10, reps=5, seed=123, title='R_markdowns/clustering_genes_03_21/cc_l1', plot='png')
+# 
+# save(cc_output, file='R_markdowns/clustering_genes_03_21/cc_l1/cc_output.RData')
+
+
+load('R_markdowns/clustering_genes_03_21/cc_l1/cc_output.RData')
 cc_clusters = cc_output[[6]]$consensusClass
 
 cc_output_c1 = datExpr_redDim %>% filter(cc_clusters==1) %>% as.matrix %>% t %>%
-   ConsensusClusterPlus(maxK=8, reps=50, seed=123, title='R_markdowns/clustering_genes_03_19/cc_l2_1/', plot='png')
-save(cc_output_c1, file='R_markdowns/clustering_genes_03_19/cc_l2_1/cc_output.RData')
+   ConsensusClusterPlus(maxK=8, reps=50, seed=123, title='R_markdowns/clustering_genes_03_21/cc_l2_1/', plot='png')
+save(cc_output_c1, file='R_markdowns/clustering_genes_03_21/cc_l2_1/cc_output.RData')
 
-cc_output_c2 = datExpr_redDim %>% filter(cc_clusters==4) %>% as.matrix %>% t %>%
-  ConsensusClusterPlus(maxK=8, reps=50, seed=123, title='R_markdowns/clustering_genes_03_19/cc_l2_2/', plot='png')
-save(cc_output_c2, file='R_markdowns/clustering_genes_03_19/cc_l2_2/cc_output.RData')
+cc_output_c2 = datExpr_redDim %>% filter(cc_clusters==2) %>% as.matrix %>% t %>%
+  ConsensusClusterPlus(maxK=8, reps=50, seed=123, title='R_markdowns/clustering_genes_03_21/cc_l2_2/', plot='png')
+save(cc_output_c2, file='R_markdowns/clustering_genes_03_21/cc_l2_2/cc_output.RData')
